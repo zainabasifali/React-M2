197b1834d8ad36a2c608bf3051fa9bfc45b7beba
zainabasifali <zainabasif5312@gmail.com>
2024-03-15 01:01:21 +0500

    List and Questionare

--- Assignment 2/src/Components/Questionare.jsx
+++ Assignment 2/src/Components/Questionare.jsx
@@ -1,52 +1,58 @@
-import React, { useState } from "react";
-var i= 0;
+import React, { useState, useEffect } from "react";
 
 function Questionare() {
-  const questions = ["Pakistan has good relationships with?", "Pakistan was named by(shortform)?"];
+  const questions = [
+    "Pakistan have how many provinces?",
+    "Pakistan has good relationships with?",
+    "Pakistan was named by?"
+  ];
   const answers = ["4", "china", "chaudry rehmat ali"];
-  
-  const [Question, setQuestion] = useState("How many provinces of Pakistan are there?");
-  const [items, setItems] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const [marks, setMarks] = useState(null); 
 
-  const Submit = () => {
-    const updatedItems = [...items, inputValue];
-    setItems(updatedItems); 
-    setInputValue(""); 
-  
-    if (i < questions.length) {
-      setQuestion(questions[i]);
-      i++;
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [marks, setMarks] = useState(0);
+
+  useEffect(() => {
+    if (currentQuestionIndex < questions.length) {
+      setMarks(null);
     }
+  }, [currentQuestionIndex]);
+
+  const handleSubmit = () => {
+    const updatedAnswers = [...userAnswers, inputValue];
+    setUserAnswers(updatedAnswers);
+    setInputValue("");
+
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } 
+    
     else {
       let userMarks = 0;
       for (let j = 0; j < answers.length; j++) {
-        if (updatedItems[j] === answers[j]) {
+        if (updatedAnswers[j] === answers[j]) {
           userMarks++;
         }
       }
       setMarks(userMarks);
     }
   };
-  
+
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
   return (
     <div>
-      <label>{Question}</label>
+      <label>{questions[currentQuestionIndex]}</label>
       <br />
-      <input type="text" placeholder="ans" value={inputValue} onChange={handleInputChange}
-      />
-      <button onClick={Submit}>Submit</button>
+      <input type="text" placeholder="ans" value={inputValue} onChange={handleInputChange}/>
+      <button onClick={handleSubmit}>Submit</button>
 
-      {<h2>Your marks are: {marks}/{answers.length}</h2>} 
+      {<h2>Your marks are: {marks}/{answers.length}</h2>}
     </div>
   );
 }
 
-export default Questionare;
-
-  
+export default Questionare
